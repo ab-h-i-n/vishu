06cab04ae80054582388169fbaefe10d451c09b3
ab-h-i-n <abhinshelly@gmail.com>
2024-04-13 23:31:54 +0530

    audio added

--- src/common/UI/HomePage.jsx
+++ src/common/UI/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import confetti from "canvas-confetti";
 import Curtains from "../components/Curtains";
 import FruitBascket from "../components/FruitBascket";
@@ -9,6 +9,8 @@ import HappyVishuText from "../components/HappyVishuText";
 const Home = () => {
   var duration = 15 * 1000;
   var skew = 1;
+  const audio = new Audio('/audio/vishu.mp3');
+  const container = useRef(null);
 
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
@@ -18,14 +20,12 @@ const Home = () => {
     var timeLeft = duration;
     var ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
-
     confetti({
       particleCount: 10,
       startVelocity: 0,
       ticks: ticks,
       origin: {
         x: Math.random(),
-        // since particles fall down, skew start toward the top
         y: Math.random() * skew - 0.2,
       },
       colors: ["#FFF633"],
@@ -34,25 +34,34 @@ const Home = () => {
       scalar: randomInRange(0.4, 1),
       drift: randomInRange(-0.4, 0.4),
     });
-
-    // Call startConfetti recursively for infinite animation
-    setTimeout(startConfetti, 500); // Adjust interval as needed
+    setTimeout(startConfetti, 500);
   }
 
   useEffect(() => {
-    // Start confetti animation after 2 seconds
     const timeoutConfetti = setTimeout(startConfetti, 2000);
 
-    // Cleanup function
+    const handleClick = () => {
+      // Play the audio when clicked anywhere on the screen
+      if(audio.paused) {
+        audio.play();
+      }else{
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    };
+
+    // Add click event listener to the window object
+    container.current.addEventListener("click", handleClick);
+
     return () => {
       clearTimeout(timeoutConfetti);
+      // Remove click event listener when component unmounts
+      container.current.removeEventListener("click", handleClick);
     };
   }, []);
 
-  
-
   return (
-    <div className="h-screen w-screen overflow-hidden">
+    <div ref={container} className="h-screen w-screen overflow-hidden">
       <Curtains />
       <FruitBascket />
       <Kanikonna />
