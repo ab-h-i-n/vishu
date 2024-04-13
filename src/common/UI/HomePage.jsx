7d2ea8f424662ee0be29082ca85f48ce691154e0
ab-h-i-n <abhinshelly@gmail.com>
2024-04-14 05:26:00 +0530

    analatics added

--- src/common/UI/HomePage.jsx
+++ src/common/UI/HomePage.jsx
@@ -1,17 +1,16 @@
-"use client"
-import React, { useEffect, useRef } from "react";
+"use client";
+import React, { useEffect } from "react";
 import confetti from "canvas-confetti";
 import Curtains from "../components/Curtains";
 import FruitBascket from "../components/FruitBascket";
 import Kanikonna from "../components/Kanikonna";
 import Krishnaa from "../components/Krishna";
 import HappyVishuText from "../components/HappyVishuText";
+import { Analytics } from "@vercel/analytics/react";
 
 const Home = () => {
   var duration = 15 * 1000;
   var skew = 1;
-  const audio = new Audio('/audio/vishu.mp3');
-  const container = useRef(null);
 
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
@@ -41,28 +40,14 @@ const Home = () => {
   useEffect(() => {
     const timeoutConfetti = setTimeout(startConfetti, 2000);
 
-    const handleClick = () => {
-      // Play the audio when clicked anywhere on the screen
-      if(audio.paused) {
-        audio.play();
-      }else{
-        audio.pause();
-        audio.currentTime = 0;
-      }
-    };
-
-    // Add click event listener to the window object
-    container.current.addEventListener("click", handleClick);
-
     return () => {
       clearTimeout(timeoutConfetti);
-      // Remove click event listener when component unmounts
-      container.current.removeEventListener("click", handleClick);
     };
   }, []);
 
   return (
-    <div ref={container} className="h-screen w-screen overflow-hidden">
+    <div className="h-screen w-screen overflow-hidden">
+      <Analytics />
       <Curtains />
       <FruitBascket />
       <Kanikonna />
