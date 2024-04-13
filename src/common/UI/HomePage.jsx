66c163f52877c93a34d4ba0cc999751a9af1cf2c
ab-h-i-n <abhinshelly@gmail.com>
2024-04-13 22:39:50 +0530

    .

--- src/common/UI/HomePage.jsx
+++ src/common/UI/HomePage.jsx
@@ -5,30 +5,25 @@ import FruitBascket from "../components/FruitBascket";
 import Kanikonna from "../components/Kanikonna";
 import Krishnaa from "../components/Krishna";
 import HappyVishuText from "../components/HappyVishuText";
-import Wish from "../components/Wish";
 
 const Home = () => {
-  const queryString = window.location.search;
-  const valueAfterQuestionMark = queryString.substring(1);
-  var duration = 15 * 1000;
-  var skew = 1;
+  const duration = 15 * 1000;
+  let skew = 1;
 
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
   }
 
   function startConfetti() {
-    var timeLeft = duration;
-    var ticks = Math.max(200, 500 * (timeLeft / duration));
+    let timeLeft = duration;
+    let ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
-
     confetti({
       particleCount: 10,
       startVelocity: 0,
       ticks: ticks,
       origin: {
         x: Math.random(),
-        // since particles fall down, skew start toward the top
         y: Math.random() * skew - 0.2,
       },
       colors: ["#FFF633"],
@@ -37,13 +32,18 @@ const Home = () => {
       scalar: randomInRange(0.4, 1),
       drift: randomInRange(-0.4, 0.4),
     });
-
     // Call startConfetti recursively for infinite animation
     setTimeout(startConfetti, 500); // Adjust interval as needed
   }
 
   useEffect(() => {
-    console.log(valueAfterQuestionMark);
+    // Check if the window object exists (client-side)
+    if (typeof window !== "undefined") {
+      const queryString = window.location.search;
+      const valueAfterQuestionMark = queryString.substring(1);
+      console.log(valueAfterQuestionMark);
+    }
+
     // Start confetti animation after 2 seconds
     const timeoutConfetti = setTimeout(startConfetti, 2000);
 
@@ -59,7 +59,7 @@ const Home = () => {
       <FruitBascket />
       <Kanikonna />
       <Krishnaa />
-      <HappyVishuText text={valueAfterQuestionMark} />
+      <HappyVishuText />
     </div>
   );
 };
